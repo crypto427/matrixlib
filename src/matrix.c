d897c062f0c7b7e678246a4bc9596c5004cf8d57
crypto427 <asaadshahmobile@gmail.com>
2026-08-28 07:14:20 -0400

    Refactor and matmul skeleton

--- src/matrix.c
+++ src/matrix.c
@@ -4,10 +4,49 @@
 
 typedef struct Matrix {
     int size;
+    int ndims;
+    int* dims;
     int* values;
 } Matrix;
 
-void init(Matrix *m, int size) {
+
+void init(Matrix *m, int dims[], int ndims);
+void print(Matrix *m);
+void matmul(Matrix *a, Matrix *b, Matrix * res);
+
+
+int main() {
+    Matrix mat, mat2, res;
+    int dims[] = {2, 2};
+    int values[] = {1, 2, 3, 4,};
+    init(&mat, dims, 2);
+    for(int i = 0; i < mat.size; i++)
+        *(mat.values+i) = values[i];
+    print(&mat);
+    int values2[] = {5, 6, 7, 8,};
+    init(&mat2, dims, 2);
+    for(int i = 0; i < mat.size; i++)
+        *(mat2.values+i) = values2[i];
+    print(&mat2);
+
+    matmul(&mat, &mat2, &res);
+
+    free((&mat)->values);
+    free((&mat)->dims);
+    free((&mat2)->values);
+    free((&mat2)->dims);
+    return 0;
+}
+
+void init(Matrix *m, int dims[], int ndims) {
+    int size = 0;
+    m->ndims = ndims;
+    m->dims = malloc(ndims * sizeof(int)); // check this todo
+    for(int i = 0; i < ndims; i++) {
+        *(m->dims+i) = dims[i];
+        size += dims[i];
+    }
+    
     m->size = size;
     m->values = malloc(size * sizeof(int)); // this needs to be checked if successful, todo later
 }
@@ -18,10 +57,10 @@ void print(Matrix *m) {
     printf("\n");
 }
 
-int main() {
-    Matrix mat;
-    init(&mat, 4);
-    print(&mat);
-    free((&mat)->values);
-    return 0;
+void matmul(Matrix *a, Matrix *b, Matrix *res)
+{
+    if((a)->dims[a->ndims-1] == b->dims[0])
+    {
+        printf("Multiplicable\n");
+    }
 }
